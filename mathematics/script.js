b261466d89d00fe2db09872da033fcde8351c58d
Souptik Samanta <161435060+souptik-samanta@users.noreply.github.com>
2024-08-25 18:37:32 +0530

    FOR MATH SCRIPT

---
+++ mathematics/script.js
@@ -0,0 +1,273 @@
+
+
+function correct(){
+    const correctAns = document.getElementById("correct"),
+          wrongAns = document.getElementById("wrong-1"),
+          wrongAns1 = document.getElementById("wrong-2"),
+          wrongAns2 = document.getElementById("wrong-3"),
+          question1 = document.querySelector(".question-1"),
+          question2 = document.querySelector(".question-2"),
+          question3 = document.querySelector(".question-3"),
+          question4 = document.querySelector(".question-4"),
+          question5 = document.querySelector(".question-5");
+     let scoreAns = document.getElementById("score");
+
+    correctAns.style.backgroundColor = "greenyellow";
+    wrongAns.style.backgroundColor = "red";
+    wrongAns1.style.backgroundColor = "red";
+    wrongAns2.style.backgroundColor = "red";
+
+    
+    setTimeout(() => {
+        question1.style.display = "none";
+        question2.style.display = "block";
+    },1000)
+
+}
+
+function wrong(){
+    const correctAns = document.getElementById("correct"),
+    wrongAns = document.getElementById("wrong-1"),
+    wrongAns1 = document.getElementById("wrong-2"),
+    wrongAns2 = document.getElementById("wrong-3"),
+    question1 = document.querySelector(".question-1"),
+    question2 = document.querySelector(".question-2"),
+    question3 = document.querySelector(".question-3"),
+    question4 = document.querySelector(".question-4"),
+    question5 = document.querySelector(".question-5");
+let scoreAns = document.getElementById("score");
+
+correctAns.style.backgroundColor = "greenyellow";
+wrongAns.style.backgroundColor = "red";
+wrongAns1.style.backgroundColor = "red";
+wrongAns2.style.backgroundColor = "red";
+
+setTimeout(() => {
+    question1.style.display = "none";
+    question2.style.display = "block";
+},1000)
+}
+function correct1(){
+    const correctAns = document.getElementById("correct-1"),
+          wrongAns = document.getElementById("wrong-4"),
+          wrongAns1 = document.getElementById("wrong-5"),
+          wrongAns2 = document.getElementById("wrong-6"),
+          question1 = document.querySelector(".question-1"),
+          question2 = document.querySelector(".question-2"),
+          question3 = document.querySelector(".question-3"),
+          question4 = document.querySelector(".question-4"),
+          question5 = document.querySelector(".question-5");
+     let scoreAns = document.getElementById("score");
+
+    correctAns.style.backgroundColor = "greenyellow";
+    wrongAns.style.backgroundColor = "#f55";
+    wrongAns1.style.backgroundColor = "#f55";
+    wrongAns2.style.backgroundColor = "#f55";
+  
+
+    setTimeout(() => {
+        question2.style.display = "none";
+        question3.style.display = "block";
+    },1000)
+
+}
+
+function wrong1(){
+    const correctAns = document.getElementById("correct-1"),
+    wrongAns = document.getElementById("wrong-4"),
+    wrongAns1 = document.getElementById("wrong-5"),
+    wrongAns2 = document.getElementById("wrong-6"),
+    question1 = document.querySelector(".question-1"),
+    question2 = document.querySelector(".question-2"),
+    question3 = document.querySelector(".question-3"),
+    question4 = document.querySelector(".question-4"),
+    question5 = document.querySelector(".question-5");
+let scoreAns = document.getElementById("score");
+
+correctAns.style.backgroundColor = "greenyellow";
+wrongAns.style.backgroundColor = "#f55";
+wrongAns1.style.backgroundColor = "#f55";
+wrongAns2.style.backgroundColor = "#f55";
+
+setTimeout(() => {
+    question2.style.display = "none";
+    question3.style.display = "block";
+},1000)
+}
+function correct2(){
+    const correctAns = document.getElementById("correct-2"),
+          wrongAns = document.getElementById("wrong-7"),
+          wrongAns1 = document.getElementById("wrong-8"),
+          wrongAns2 = document.getElementById("wrong-9"),
+          question1 = document.querySelector(".question-1"),
+          question2 = document.querySelector(".question-2"),
+          question3 = document.querySelector(".question-3"),
+          question4 = document.querySelector(".question-4"),
+          question5 = document.querySelector(".question-5");
+     let scoreAns = document.getElementById("score");
+
+    correctAns.style.backgroundColor = "greenyellow";
+    wrongAns.style.backgroundColor = "#f55";
+    wrongAns1.style.backgroundColor = "#f55";
+    wrongAns2.style.backgroundColor = "#f55";
+
+   
+    setTimeout(() => {
+        question3.style.display = "none";
+        question4.style.display = "block";
+    },1000)
+
+}
+
+function wrong2(){
+    const correctAns = document.getElementById("correct-2"),
+    wrongAns = document.getElementById("wrong-7"),
+    wrongAns1 = document.getElementById("wrong-8"),
+    wrongAns2 = document.getElementById("wrong-9"),
+    question1 = document.querySelector(".question-1"),
+    question2 = document.querySelector(".question-2"),
+    question3 = document.querySelector(".question-3"),
+    question4 = document.querySelector(".question-4"),
+    question5 = document.querySelector(".question-5");
+let scoreAns = document.getElementById("score");
+
+correctAns.style.backgroundColor = "greenyellow";
+wrongAns.style.backgroundColor = "#f55";
+wrongAns1.style.backgroundColor = "#f55";
+wrongAns2.style.backgroundColor = "#f55";
+
+setTimeout(() => {
+    question3.style.display = "none";
+    question4.style.display = "block";
+},1000)
+}
+function correct3(){
+    const correctAns = document.getElementById("correct-3"),
+          wrongAns = document.getElementById("wrong-10"),
+          wrongAns1 = document.getElementById("wrong-11"),
+          wrongAns2 = document.getElementById("wrong-12"),
+          question1 = document.querySelector(".question-1"),
+          question2 = document.querySelector(".question-2"),
+          question3 = document.querySelector(".question-3"),
+          question4 = document.querySelector(".question-4"),
+          question5 = document.querySelector(".question-5");
+     let scoreAns = document.getElementById("score");
+
+    correctAns.style.backgroundColor = "greenyellow";
+    wrongAns.style.backgroundColor = "#f55";
+    wrongAns1.style.backgroundColor = "#f55";
+    wrongAns2.style.backgroundColor = "#f55";
+
+    setTimeout(() => {
+        question4.style.display = "none";
+        question5.style.display = "block";
+    },1000)
+
+}
+
+function wrong3(){
+    const correctAns = document.getElementById("correct-3"),
+    wrongAns = document.getElementById("wrong-10"),
+    wrongAns1 = document.getElementById("wrong-11"),
+    wrongAns2 = document.getElementById("wrong-12"),
+    question1 = document.querySelector(".question-1"),
+    question2 = document.querySelector(".question-2"),
+    question3 = document.querySelector(".question-3"),
+    question4 = document.querySelector(".question-4"),
+    question5 = document.querySelector(".question-5");
+let scoreAns = document.getElementById("score");
+
+correctAns.style.backgroundColor = "greenyellow";
+wrongAns.style.backgroundColor = "#f55";
+wrongAns1.style.backgroundColor = "#f55";
+wrongAns2.style.backgroundColor = "#f55";
+
+setTimeout(() => {
+    question4.style.display = "none";
+    question5.style.display = "block";
+},1000)
+}
+function correct4(){
+    const correctAns = document.getElementById("correct-4"),
+          wrongAns = document.getElementById("wrong-13"),
+          wrongAns1 = document.getElementById("wrong-14"),
+          wrongAns2 = document.getElementById("wrong-15"),
+          question1 = document.querySelector(".question-1"),
+          question2 = document.querySelector(".question-2"),
+          question3 = document.querySelector(".question-3"),
+          question4 = document.querySelector(".question-4"),
+          question5 = document.querySelector(".question-5");
+     let scoreAns = document.getElementById("score");
+
+     
+
+    correctAns.style.backgroundColor = "greenyellow";
+    wrongAns.style.backgroundColor = "#f55";
+    wrongAns1.style.backgroundColor = "#f55";
+    wrongAns2.style.backgroundColor = "#f55";
+
+   
+    setTimeout(() => {
+        question5.style.display = "none";
+        document.querySelector(".thanks-for-completing").style.display = "block";
+    },1000)
+ 
+}
+
+function wrong4(){
+    const correctAns = document.getElementById("correct-4"),
+    wrongAns = document.getElementById("wrong-13"),
+    wrongAns1 = document.getElementById("wrong-14"),
+    wrongAns2 = document.getElementById("wrong-15"),
+    question1 = document.querySelector(".question-1"),
+    question2 = document.querySelector(".question-2"),
+    question3 = document.querySelector(".question-3"),
+    question4 = document.querySelector(".question-4"),
+    question5 = document.querySelector(".question-5");
+let scoreAns = document.getElementById("score");
+
+correctAns.style.backgroundColor = "greenyellow";
+wrongAns.style.backgroundColor = "#f55";
+wrongAns1.style.backgroundColor = "#f55";
+wrongAns2.style.backgroundColor = "#f55";
+    
+question5.style.display = "none";
+document.querySelector(".question-panel").style.display = "none";
+    document.querySelector(".thanks-for-completing").style.display = "block";
+
+}
+
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    var fiveMinutes = 60 * 5,
+        display = document.querySelector('#time');
+    startTimer(fiveMinutes, display);
+};
+
+if(document.querySelector("#time").innerHTML == "00:00"){
+    alert('time up!');
+}
+
+
+
+
+
+
+
+
